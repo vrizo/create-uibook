ea202ee4849ba404c54f121d0b021ef934bbf1db
vrizo <kb@kernel-it.ru>
2019-03-08 21:10:17 +0300

    Add example files and copy them during initialization

--- create-uibook.js
+++ create-uibook.js
@@ -1,10 +1,22 @@
 #!/usr/bin/env node
+let fs = require('fs')
+let dir = 'uibook-example'
 
-console.log('The script do nothing...')
+console.log('Creating Uibook example structure')
 
-var fs = require('fs');
-var dir = 'uibook-example';
-
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir)
 }
+
+fs.copyFile('src/button.uibook.js', dir + '/button.uibook.js', err => {
+  if (err) throw err
+  console.log('Created: ' + dir + '/button.uibook.js')
+});
+
+fs.copyFile('src/uibook-controller.js', dir + '/uibook-controller.js', err => {
+  if (err) throw err
+  console.log('Created: ' + dir + '/uibook-controller.js')
+})
+
+console.log('For more information please refer to Uibook Docs: ' +
+  'https://github.com/vrizo/uibook/')
