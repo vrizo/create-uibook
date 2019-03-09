087841b1839556962c03a49ea3af8060582d452e
vrizo <kb@kernel-it.ru>
2019-03-09 19:24:18 +0300

    Use __dirname to copy files

--- create-uibook.js
+++ create-uibook.js
@@ -1,27 +1,23 @@
 #!/usr/bin/env node
+let path = require('path')
 let fs = require('fs')
-let dir = 'uibook-example'
-
-console.log('Creating Uibook example structure')
-
-console.log('DEBUG. process.env.HOME: ', process.env.HOME)
-console.log('DEBUG. process.cwd(): ', process.cwd())
-console.log('DEBUG. process.argv: ', process.argv)
-console.log('DEBUG. process.env: ', process.env)
-console.log('DEBUG. __dirname: ', __dirname)
+let dir = 'uibook'
 
 if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir)
 }
 
-fs.copyFile('src/button.uibook.js', dir + '/button.uibook.js', err => {
-  if (err) throw err
-  console.log('Created: ' + dir + '/button.uibook.js')
-});
+const CONTROLLER = path.join(__dirname, 'src', 'uibook-controller.js')
+const BUTTON = path.join(__dirname, 'src', 'button.uibook.js')
 
-fs.copyFile('src/uibook-controller.js', dir + '/uibook-controller.js', err => {
+fs.copyFile(CONTROLLER, dir + '/uibook-controller.js', err => {
   if (err) throw err
   console.log('Created: ' + dir + '/uibook-controller.js')
+})
+
+fs.copyFile(BUTTON, dir + '/button.uibook.js', err => {
+  if (err) throw err
+  console.log('Created: ' + dir + '/button.uibook.js')
 })
 
 console.log('For more information please refer to Uibook Docs: ' +
